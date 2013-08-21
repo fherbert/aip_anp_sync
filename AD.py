6a81f1544484e3266ca4c8a0a9aac3a711fb506f
Francois Herbert <F.Herbert@massey.ac.nz>
2013-08-21 14:34:42 +1200

    Working

--- AD.py
+++ AD.py
@@ -6,21 +6,26 @@ AIPBASEURL = "http://www.aip.net.nz/"
 
 def add_single_ad(line, aerodrome=None):
    """ Gets a line of scraped html and returns an AD object """
-   m = re.search('href="pdf/(.*)&#xD(.*)>(.*)</a>(.*)\((.*)\)', line)
    if not aerodrome:
+      m = re.search('href="pdf/(.*)&#xD(.*)>(.*)</a>(.*)\((.*)\)', line)
       aerodrome = AD(name = m.group(3), ICAO_code = m.group(5))
-   aerodrome.assets.append(ad_asset(name = m.group(3).strip(), filename = m.group(1)))  
+   else:
+      m = re.search('href="pdf/(.*)&#xD(.*)>(.*)</a>', line)
+   aerodrome.assets.append(ad_asset(name = m.group(3).strip().replace("&amp;", "&"), filename = m.group(1)))  
    return aerodrome
 
 def add_multiple_ad(line):
    """ gets a multiple pdf aerodrome """
    m=re.search('href="(.*)">(.*)</a>(.*)\((.*)\)', line)
+   print "Creating aerodrome with name %s" % m.group(2).strip()
    aerodrome = AD(name=m.group(2).strip(), ICAO_code=m.group(4))
    # now we need to get the list of pdf's
-   response = urllib2.urlopen(AIPBASEURL + m.group(1))
+   response = urllib2.urlopen(AIPBASEURL + m.group(1).replace("&amp;", "&"))
+   print "Getting all assets for %s at %s" % (aerodrome.name, AIPBASEURL + m.group(1).replace("&amp;", "&"))
    for line in response:
       if 'pdf' in line:
          add_single_ad(line, aerodrome)   
+   return aerodrome
 
 class AD:
    def __init__(self, name, ICAO_code):
--- getaip.py
+++ getaip.py
@@ -12,16 +12,13 @@ if response.code != 200:
    print "Error connecting to AIP site"
    sys.exit(0)
 
-raw_ad_single_pdf = []
-raw_ad_multiple_pdf = []
-
 for line in response:
    if 'pdf' in line:
       aerodromes.append(add_single_ad(line.strip()))
-
    if 'section=CHARTS&amp' in line:
       aerodromes.append(add_multiple_ad(line.strip()))
 
+# now we have a full list of aerodromes with all assets
 
 
    
